a3b0b2224da7b704615423f662170817c5b9e834
Sushma <nellisushma@gmail.com>
2023-06-06 15:35:07 -0500

    Roboshop nginx Script

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,20 @@
 echo -e "\e[35mInstalling nginx\e[0m"
-yum install nginx -y
+yum install nginx -y >/tmp/roboshop.log
 
 echo -e "\e[35mRemoving old app. content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[35mDownloading frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
 
 echo -e "\e[35mExtracting the content\e[0m"
 cd /usr/share/nginx/html
 
 echo -e "\e[35Unzipping the content\e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >/tmp/roboshop.log
 
 #we need to copy config file
 
 echo -e "\e[35mStarting nginx server\e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx >/tmp/roboshop.log
+systemctl restart nginx >/tmp/roboshop.log
