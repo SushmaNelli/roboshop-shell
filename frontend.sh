c7b1056d40bbc354f4af737a986e0ffa377c047a
Sushma <nellisushma@gmail.com>
2023-08-06 18:21:45 -0500

    Roboshop nginx Script

--- frontend.sh
+++ frontend.sh
@@ -11,7 +11,8 @@ echo -e "\e[35mExtracting the content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-#we need to copy config file
+echo -e "\e[35mUpdate Frontend Configuration\e[0m"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[35mStarting nginx server\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
