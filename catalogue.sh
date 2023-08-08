528bf5771a83cd69214d03cb3f6bbd266e81bf05
Sushma <nellisushma@gmail.com>
2023-08-07 23:26:02 -0500

    Roboshop nginx Script

--- catalogue.sh
+++ catalogue.sh
@@ -23,7 +23,7 @@ echo -e "\e[33mInstall NodeJS Dependencies\e[0m"
 npm install &>>/tmp/roboshop.log
 
 echo -e "\e[33mSetup SystemD Service\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
 echo -e "\e[33mStart Catalogue Service\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
@@ -31,7 +31,7 @@ systemctl enable catalogue &>>/tmp/roboshop.log
 systemctl start catalogue &>>/tmp/roboshop.log
 
 echo -e "\e[33mCopy Mongodb Repo file\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstall Mongodb-client\e[0m"
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
