93562695b197ba11aa89d7685380c370f0793132
Sushma <nellisushma@gmail.com>
2023-08-10 00:18:33 -0500

    Roboshop nginx Script

---
+++ cart.sh
@@ -0,0 +1,31 @@
+echo -e "\e[33mConfiguring NodeJS Repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+
+echo -e "\e[33mInstall NodeJS\e[0m"
+yum install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[33mAdd Application User\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[33mRemove Old Content and Create Application Directory\e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app
+
+echo -e "\e[33mDownload Application Content\e[0m"
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
+cd /app
+
+echo -e "\e[33mExtract Application Content\e[0m"
+unzip /tmp/cart.zip &>>/tmp/roboshop.log
+cd /app
+
+echo -e "\e[33mInstall NodeJS Dependencies\e[0m"
+npm install &>>/tmp/roboshop.log
+
+echo -e "\e[33mSetup SystemD Service\e[0m"
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
+
+echo -e "\e[33mStart Cart Service\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable cart &>>/tmp/roboshop.log
+systemctl restart cart &>>/tmp/roboshop.log
