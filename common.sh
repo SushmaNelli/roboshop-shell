9c407c68684543bde7cdf38edf796a83e5e90797
Sushma <nellisushma@gmail.com>
2023-08-11 18:27:39 -0500

    functinos

--- common.sh
+++ common.sh
@@ -1,8 +1,35 @@
 color="\e[31m"
-nocolor="\e[0m"
+nocolor="${nocolor}"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 
+app_presetup() {
+  echo -e "${color} Add Application User ${nocolor}"
+    useradd roboshop &>>${log_file}
+
+    echo -e "${color} Remove Old Content and Create Application Directory ${nocolor}"
+    rm -rf ${app_path} &>>${log_file}
+    mkdir ${app_path}
+
+    echo -e "${color} Download Application Content ${nocolor}"
+    curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
+    cd ${app_path}
+
+    echo -e "${color} Extract Application Content ${nocolor}"
+    unzip /tmp/$component.zip &>>${log_file}
+    cd ${app_path}
+}
+
+systemd_setup() {
+
+  echo -e "${color} Setup SystemD Service ${nocolor}"
+  cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log_file}
+
+  echo -e "${color}Start $component service.${nocolor}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable $component &>>${log_file}
+  systemctl restart $component &>>${log_file}
+}
 
 nodejs() {
   echo -e "${color} Configuring NodeJS Repos ${nocolor}"
@@ -11,31 +38,12 @@ nodejs() {
   echo -e "${color} Install NodeJS ${nocolor}"
   yum install nodejs -y &>>${log_file}
 
-  echo -e "${color} Add Application User ${nocolor}"
-  useradd roboshop &>>${log_file}
-
-  echo -e "${color} Remove Old Content and Create Application Directory ${nocolor}"
-  rm -rf ${app_path} &>>${log_file}
-  mkdir ${app_path}
-
-  echo -e "${color} Download Application Content ${nocolor}"
-  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
-  cd ${app_path}
-
-  echo -e "${color} Extract Application Content ${nocolor}"
-  unzip /tmp/$component.zip &>>${log_file}
-  cd ${app_path}
+  app_presetup
 
   echo -e "${color} Install NodeJS Dependencies ${nocolor}"
   npm install &>>${log_file}
 
-  echo -e "${color} Setup SystemD Service ${nocolor}"
-  cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log_file}
-
-  echo -e "${color} Start $component Service ${nocolor}"
-  systemctl daemon-reload &>>${log_file}
-  systemctl enable $component &>>${log_file}
-  systemctl restart $component &>>${log_file}
+  systemd_setup
 
 }
 
@@ -48,4 +56,27 @@ mongo_schema_setup() {
 
   echo -e "${color}Load Schema${nocolor}"
   mongo --host mongodb-dev.sushma1923.pics <${app_path}/schema/$component.js &>>${log_file}
+}
+
+mysql_schema_setup() {
+   echo -e "${color}Install Mysql Client${nocolor}"
+    yum install mysql -y &>>${log_file}
+
+    echo -e "${color} Load Schema${nocolor}"
+    mysql -h mysql-dev.sushma1923.pics -uroot -pRoboShop@1 < ${app_path}/schema/$component.sql &>>${log_file}
+}
+
+maven() {
+  echo -e "${color}Install Maven${nocolor}"
+  yum install maven -y &>>${log_file}
+
+  app_presetup
+
+  echo -e "${color}Download Maven dependencies ${nocolor}"
+  mvn clean package &>>${log_file}
+  mv target/$component-1.0.jar $component.jar &>>${log_file}
+
+  mysql_schema_setup
+
+  systemd_setup
 }
