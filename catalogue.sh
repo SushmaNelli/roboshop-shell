39a968cc11be809493ac854b89675970d0fb5748
Sushma <nellisushma@gmail.com>
2023-08-11 14:35:54 -0500

    variables

--- catalogue.sh
+++ catalogue.sh
@@ -1,46 +1,46 @@
 component=catalogue
 color="\e[36m"
 nocolor="\e[0m"
-log-file="/tmp/roboshop.log"
-app-path="/app"
+log_file="/tmp/roboshop.log"
+app_path="/app"
 
 echo -e "${color} Configuring NodeJS Repos ${nocolor}"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log-file}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
 echo -e "${color} Install NodeJS ${nocolor}"
-yum install nodejs -y &>>${log-file}
+yum install nodejs -y &>>${log_file}
 
 echo -e "${color} Add Application User ${nocolor}"
-useradd roboshop &>>${log-file}
+useradd roboshop &>>${log_file}
 
 echo -e "${color} Remove Old Content and Create Application Directory ${nocolor}"
-rm -rf ${app-path} &>>${log-file}
-mkdir ${app-path}
+rm -rf ${app_path} &>>${log_file}
+mkdir ${app_path}
 
 echo -e "${color} Download Application Content ${nocolor}"
-curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log-file}
-cd ${app-path}
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
+cd ${app_path}
 
 echo -e "${color} Extract Application Content ${nocolor}"
-unzip /tmp/$component.zip &>>${log-file}
-cd ${app-path}
+unzip /tmp/$component.zip &>>${log_file}
+cd ${app_path}
 
 echo -e "${color} Install NodeJS Dependencies ${nocolor}"
-npm install &>>${log-file}
+npm install &>>${log_file}
 
 echo -e "${color} Setup SystemD Service ${nocolor}"
-cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log-file}
+cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log_file}
 
 echo -e "${color} Start $component Service ${nocolor}"
-systemctl daemon-reload &>>${log-file}
-systemctl enable $component &>>${log-file}
-systemctl restart $component &>>${log-file}
+systemctl daemon-reload &>>${log_file}
+systemctl enable $component &>>${log_file}
+systemctl restart $component &>>${log_file}
 
 echo -e "${color}Copy Mongodb Repo file${nocolor}"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log-file}
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 echo -e "${color}Install Mongodb-client${nocolor}"
-yum install mongodb-org-shell -y &>>${log-file}
+yum install mongodb-org-shell -y &>>${log_file}
 
 echo -e "${color}Load Schema${nocolor}"
-mongo --host mongodb-dev.sushma1923.pics <${app-path}/schema/$component.js &>>${log-file}
+mongo --host mongodb-dev.sushma1923.pics <${app_path}/schema/$component.js &>>${log_file}
