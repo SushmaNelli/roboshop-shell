f2d142039bb5eb75ef800846057ecd4cd0cf6838
Sushma <nellisushma@gmail.com>
2023-08-18 00:05:49 -0500

    functinos

--- common.sh
+++ common.sh
@@ -45,8 +45,9 @@ systemd_setup() {
 
   echo -e "${color} Setup SystemD Service ${nocolor}"
   cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log_file}
-
+   sed -i -e "s/roboshop_app_password/$roboshop_app_password/" /home/centos/roboshop-shell/$component.service
   stat_check $?
+
 
   echo -e "${color}Start $component service.${nocolor}"
   systemctl daemon-reload &>>${log_file}
@@ -121,6 +122,5 @@ python() {
 
 stat_check $?
 
-sed -i -e "s/roboshop_app_password/$1/" /home/centos/roboshop-shell/$component.service
  systemd_setup
 }
