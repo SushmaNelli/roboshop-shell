4a5b4a0fed4b739058f103c3c2fc97d830429ac8
Sushma <nellisushma@gmail.com>
2023-08-21 18:16:07 -0500

    functinos

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,23 +1,23 @@
 source common.sh
 
-echo -e "${color}}Configure Erlang repos${nocolor}}"
+echo -e "${color}Configure Erlang repos${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log_file}
  stat_check $?
 
-echo -e "${color}}Configure Rabbitmq Repos${nocolor}}"
+echo -e "${color}Configure Rabbitmq Repos${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log_file}
  stat_check $?
 
-echo -e "${color}}Install Rabbitmq Server${nocolor}}"
+echo -e "${color}Install Rabbitmq Server${nocolor}"
 yum install rabbitmq-server -y &>>${log_file}
  stat_check $?
 
-echo -e "${color}}Start Rabbitmq Service${nocolor}}"
+echo -e "${color}Start Rabbitmq Service${nocolor}"
 systemctl enable rabbitmq-server &>>${log_file}
 systemctl restart rabbitmq-server &>>${log_file}
  stat_check $?
 
-echo -e "${color}}Add Rabbitmq Application user${nocolor}}"
+echo -e "${color}Add Rabbitmq Application user${nocolor}"
 rabbitmqctl add_user roboshop $1 &>>${log_file}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log_file}
  stat_check $?
