4fc0952391e9c0b5b14158561969dbcebedea289
Sushma <nellisushma@gmail.com>
2023-08-22 16:16:57 -0500

    functinos

--- redis.sh
+++ redis.sh
@@ -18,5 +18,5 @@ sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_fil
 
 echo -e "${color}Start Redis Service${nocolor}"
 systemctl enable redis &>>${log_file}
-systemctl start redis &>>${log_file}
+systemctl restart redis &>>${log_file}
  stat_check $?
