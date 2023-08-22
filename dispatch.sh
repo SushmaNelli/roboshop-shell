e0e6b6cd6da3b0c48695685baf176b6e60dc53ea
Sushma <nellisushma@gmail.com>
2023-08-22 16:46:56 -0500

    functinos

--- dispatch.sh
+++ dispatch.sh
@@ -1,6 +1,12 @@
 source common.sh
 component=dispatch
 
+roboshop_app_password=$1
+if [ -z "$roboshop_app_password" ]; then
+  echo "roboshop_app_password is missing"
+  exit 1
+  fi
+
 echo -e "${color}Install GoLang${nocolor}"
 yum install golang -y &>>${log_file}
  stat_check $?
