6b730af274fa72013c361eb18ceafd8a6378f57f
Sushma <nellisushma@gmail.com>
2023-08-22 15:59:21 -0500

    functinos

--- dispatch.sh
+++ dispatch.sh
@@ -1,4 +1,5 @@
 source common.sh
+component=dispatch
 
 echo -e "${color}Install GoLang${nocolor}"
 yum install golang -y &>>${log_file}
@@ -7,17 +8,9 @@ yum install golang -y &>>${log_file}
 app_presetup
 
 echo -e "${color}Download the dependencies${nocolor}"
-go mod init dispatch &>>${log_file}
+go mod init $component &>>${log_file}
 go get &>>${log_file}
 go build &>>${log_file}
  stat_check $?
 
-echo -e "${color}Setup SystemD Dispatch Service${nocolor}"
-cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service &>>${log_file}
- stat_check $?
-
-echo -e "${color}Start Dispatch service${nocolor}"
-systemctl daemon-reload &>>${log_file}
-systemctl enable dispatch &>>${log_file}
-systemctl restart dispatch &>>${log_file}
- stat_check $?
+systemd_setup
